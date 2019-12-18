bc373f1aa0bd9056f1797c350a41eb449f9d8b83
Scott Taylor <scottntaylor21@gmail.com>
2019-12-17 18:09:29 -0800

    routes created

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,23 @@
 var book = require("..models/burger.js");
+
+module.exports = function (app){
+    app.get("/api/all", function (req, res){
+        burger.findAll({}).then(function(results){
+            res.json(results);
+        })
+    });
+
+    app.post("/api/new", function(req, res){
+        
+    });
+
+    app.delete("/api/burger/:id", function(req, res){
+        burger.destroy({
+            where: {
+                burger_name: req.params.burger_name
+            }
+        }).then(function (results){
+            res.json(results);
+        });
+    })
+}
